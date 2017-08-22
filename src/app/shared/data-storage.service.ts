6d95d198596fd9b3468f834f5d31e8d9b11b953a
sigitamocevice <sigita.z@gmail.com>
2017-08-22 10:38:22 +0300

    map panaudojimas SVARBU import rxjs

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,29 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service'
 import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
-
-constructor(private http: Http,     
-            private recipeService: RecipeService) {} 
+constructor(private http: Http, private recipeService: RecipeService) {} 
 
 storeRecipes() {
-    return this.http.put(
-        'https://ng-recipe-http.firebaseio.com/recipes.json',
-        this.recipeService.getRecipes());
+    return this.http.put('https://ng-recipe-http.firebaseio.com/recipes.json', this.recipeService.getRecipes());
 }
 
 fetchRecipes() {
     this.http.get('https://ng-recipe-http.firebaseio.com/recipes.json')
-        .subscribe(
+        .map(
             (response: Response) => {
                 const recipes: Recipe[] = response.json();
+                for ( let recipe of recipes ) {
+                    if (!recipe['ingredients']) {
+                        console.log(recipe);
+                        recipe['ingredients'] = [];
+                    }
+                }
+                return recipes;
+            }
+        )
+        .subscribe(
+            (recipes: Recipe[]) => {
                 this.recipeService.setRecipes(recipes);
             }
         );
-}
+    }
 
 }
