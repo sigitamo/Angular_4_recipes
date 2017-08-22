9ebc9a4707328581cd251ce9a37d0d98d6d27948
sigitamocevice <sigita.z@gmail.com>
2017-08-22 14:34:39 +0300

    signup baigtas

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,12 @@
+import * as firebase from 'firebase';
 
 export class AuthService {
 
     signupUser(email: string, password: string) {
-
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(
+                error => console.log(error) 
+            )
     }
 
 }
